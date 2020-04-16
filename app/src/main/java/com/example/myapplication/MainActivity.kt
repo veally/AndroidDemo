0c582f74d15913f9ae1fd3f09c24fe42dae4d9c2
veally <veally@veallys-Mac-15.local>
2020-04-16 13:27:11 +0800

    修改ID

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        sv_main_score.setData(5f, 10f)
         dsv_score.setText("Hello", 20, 20, true)
 //        dsv_score2.setText("你说呢2", 50, 100, true)
     }
